d04a9dbb258e75376c389bf59a605b9e9d4a0ebd
diegogarrido <chicodie04@gmail.com>
2017-06-16 23:11:36 -0400

    Delete web.js

--- colegioWEB/angular/web.js
+++
@@ -1,8 +0,0 @@
-angular.module('mdButton', ['ngRoute','ngMaterial']);
-
-angular.module('mdButton', ['ngRoute','ngMaterial'])
-.controller('mdButton', function($scope) {
-  $scope.title1 = 'Button';
-  $scope.title4 = 'Warn';
-  $scope.isDisabled = true;
-});
