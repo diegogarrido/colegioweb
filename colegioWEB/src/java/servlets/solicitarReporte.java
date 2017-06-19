1ef15bbfc798219b48c63c850091afd358dfdf5e
dgarrido04 <d.garrido04@ufromail.cl>
2017-06-19 00:05:10 -0400

    actualizado servlet reportes

--- colegioWEB/src/java/servlets/solicitarReporte.java
+++ colegioWEB/src/java/servlets/solicitarReporte.java
@@ -42,42 +42,47 @@ public class solicitarReporte extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String curso = request.getParameter("curso");
-        String tipo = "null";
-        if (request.getParameter("reportType") != null) {
-            tipo = request.getParameter("reportType");
-        }
-        switch (tipo) {
-            case ("null"):
-                /*cargar y enviar el curso como objeto quizás?
+        try {
+            String curso = request.getParameter("curso");
+            String tipo = "null";
+            if (request.getParameter("reportType") != null) {
+                tipo = request.getParameter("reportType");
+            }
+            switch (tipo) {
+                case ("null"):
+                    /*cargar y enviar el curso como objeto quizás?
                 RetrieveCurso r = new RetrieveCurso();
                 Curso cur = r.retrieveCurso(curso);
                 request.setAttribute("objCurso",cur);
-                 */
-                request.setAttribute("curso", curso);
-                request.getRequestDispatcher("solicitarReporte.jsp").forward(request, response);
-                break;
-            case ("reprobados"):
-                Curso cur = retrieveCurso.retrieveCurso(curso);
-                ArrayList<Object> datos = new ArrayList();
-                datos.add(new Object[]{"Alumnos del curso " + curso + " en estado de repitencia"});
-                for (int i = 0; i < cur.getAlumnos().size(); i++) {
-                    if (cur.getAlumnos().get(i).estaReprobando()) {
-                        datos.add(new Object[]{cur.getAlumnos().get(i).getNombre() + " por " + cur.getAlumnos().get(i).razonReprobado()});
-                    }
-                }
-                String[] url = new String[4];
-                ServletContext s = getServletContext();
-                if (reportes.word(datos, "reprobados" + curso.replaceAll(" ", ""), s.getRealPath("/xslt/word.xsl")).contains("Exito")) {
-                    url[0] = "reportes/reprobados" + curso.replaceAll(" ", "") + ".doc";
-                    request.setAttribute("url", url);
+                     */
                     request.setAttribute("curso", curso);
-                    request.getRequestDispatcher("reportReprobados.jsp").forward(request, response);
-                }else{
-                    request.setAttribute("msg", "Error");
-                    request.getRequestDispatcher("mensaje.jsp").forward(request, response);
-                }
-                break;
+                    request.getRequestDispatcher("solicitarReporte.jsp").forward(request, response);
+                    break;
+                case ("reprobados"):
+                    Curso cur = retrieveCurso.retrieveCurso(curso);
+                    ArrayList<Object> datos = new ArrayList();
+                    datos.add(new Object[]{"Alumnos del curso " + curso + " en estado de repitencia"});
+                    for (int i = 0; i < cur.getAlumnos().size(); i++) {
+                        if (cur.getAlumnos().get(i).estaReprobando()) {
+                            datos.add(new Object[]{cur.getAlumnos().get(i).getNombre() + " por " + cur.getAlumnos().get(i).razonReprobado()});
+                        }
+                    }
+                    String[] url = new String[4];
+                    ServletContext s = getServletContext();
+                    if (reportes.word(datos, "reprobados" + curso.replaceAll(" ", ""), s.getRealPath("/xslt/word.xsl")).contains("Exito")) {
+                        url[0] = "reportes/reprobados" + curso.replaceAll(" ", "") + ".doc";
+                        request.setAttribute("url", url);
+                        request.setAttribute("curso", curso);
+                        request.getRequestDispatcher("reportReprobados.jsp").forward(request, response);
+                    } else {
+                        request.setAttribute("msg", "Error");
+                        request.getRequestDispatcher("mensaje.jsp").forward(request, response);
+                    }
+                    break;
+            }
+        } catch (Exception e) {
+            request.setAttribute("msg", "Error "+e.getLocalizedMessage());
+            request.getRequestDispatcher("mensaje.jsp").forward(request, response);
         }
     }
 
